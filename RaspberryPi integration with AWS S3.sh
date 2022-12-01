a1abc286aa4dc76792c05236cea493a82c2eac3c
Hithesh Reddy Yaduguri <75219792+YHR2411@users.noreply.github.com>
2022-12-01 22:41:34 +0530

    Update RaspberryPi integration with AWS S3.sh

--- RaspberryPi integration with AWS S3.sh
+++ RaspberryPi integration with AWS S3.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Installing AWSCLI on Raspberry Pi
+sudo apt-get install awscli -y
+aws --version
+
+# Defining variables to use them in the commands below
 access_key="ENTER_YOUR_AWS_ACCESSKEY_HERE"
 secret_access_key="ENTER_YOUR_AWS_SECRET_ACCESSKEY_HERE"
 region="ENTER_YOUR_REGION_HERE"
 bucket_name="ENTER_THE_NAME_OF_THE_S3_BUCKET_HERE"
 mount_path="/home/pi/bucket"    # Change the path with the mount path you would like to configure
 
+# aws configuration through awscli
 aws configure set default.region $region    # Configuring region for AWSCLI
 aws configure set aws_access_key_id $access_key   # Configuring AWS Accesskey
 aws configure set aws_secret_access_key $secret_access_key    # Configurong AWS Secret Accesskey
 
 # Synchronizing the mount path with AWS S3 Bucket
+# Changes made in mount path are reflected in bucket and vice-versa
 aws s3 sync $mount_path s3://$bucket_name
 echo $access_key:$secret_access_key > "$HOME/.passwd_s3fs"
 chmod 600 "$HOME/.passwd_s3fs"
