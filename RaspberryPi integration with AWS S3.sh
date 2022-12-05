b7be592e8471df1d2341d5026ccce41ec174d195
Hithesh Reddy Yaduguri <75219792+YHR2411@users.noreply.github.com>
2022-12-05 13:57:02 +0530

    Update RaspberryPi integration with AWS S3.sh

--- RaspberryPi integration with AWS S3.sh
+++ RaspberryPi integration with AWS S3.sh
@@ -21,7 +21,8 @@ aws configure set aws_secret_access_key $secret_access_key    # Configurong AWS 
 aws s3 sync $mount_path s3://$bucket_name
 echo $access_key:$secret_access_key > "$HOME/.passwd_s3fs"
 chmod 600 "$HOME/.passwd_s3fs"
-s3fs $bucket_name $mount_path -o passwd_file="${HOME}/.passwd_s3fs",nonempty,allow_other,mp_umask=002,uid=1000,gid=1000 -o url=http://s3.$region.amazonaws.com,endpoint=$region,use_path_re>echo $bucket_name $mount_path fuse.s3fs _netdev,allow_other 0 0 >> "/etc/fstab"
+s3fs $bucket_name $mount_path -o passwd_file="${HOME}/.passwd_s3fs",nonempty,allow_other,mp_umask=002,uid=1000,gid=1000 -o url=http://s3.$region.amazonaws.com,endpoint=$region,use_path_request_style
+echo $bucket_name $mount_path fuse.s3fs _netdev,allow_other 0 0 >> "/etc/fstab"
 
 # Confirmation Message
 echo Mount location $mount_path successfully connected to the bucket $bucket_name
